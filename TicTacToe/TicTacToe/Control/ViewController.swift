9ac170d53b3c4bfc7bbb83759b458d94bfd7cd8f
alysonabril <alysonabril@pursuit.org>
2019-08-08 19:20:58 -0400

    completed newGameButton pressed, resets game board

--- TicTacToe/TicTacToe/Control/ViewController.swift
+++ TicTacToe/TicTacToe/Control/ViewController.swift
@@ -9,38 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var brain = TicTacToeBrain()
     
     
     @IBOutlet weak var playerTurnLabel: UILabel!
-    
+    @IBOutlet weak var newGameButton: UIButton!
     
     override func viewDidLoad() {
-    super.viewDidLoad()
-        
-  }
-
-
+        super.viewDidLoad()
+        playerTurnLabel.isHidden = true
+//        newGameButton.isHidden = true
+    }
+    
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         if brain.playerTurn ==  .one {
             
-            updateTurn(title: "X", text: "Go for it, Player 1!", color: .blue, button: sender)
+            updateTurn(title: "X", text: "Player 2, make your move!", color: .magenta, button: sender)
             brain.updatePlayerTurn()
             
         } else if brain.playerTurn ==  .two {
             
-            updateTurn(title: "O", text: "Player 2, make your move!", color: .magenta, button: sender)
+            
+            updateTurn(title: "O", text: "Go for it, Player 1!", color: .blue, button: sender)
             brain.updatePlayerTurn()
         }
         
+    }
+    
+    
+    @IBAction func newGameButtonPressed(_ sender: UIButton) {
+        
+        playerTurnLabel.text = "Please Make a move Player 1"
+        
+        for num in 1...9 {
+            let button = view.viewWithTag(num) as! UIButton
+            button.setTitle("", for: .normal)
+        }
     }
     
     func updateTurn(title: String, text: String, color: UIColor, button: UIButton) {
         button.setTitle(title, for: .normal)
         button.setTitleColor(color, for: .normal)
         playerTurnLabel.text = text
+        playerTurnLabel.isHidden = false
         button.isEnabled = false
         
     }
